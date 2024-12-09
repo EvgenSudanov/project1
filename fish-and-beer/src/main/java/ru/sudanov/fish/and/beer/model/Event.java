4e4c58c24fabbe3d2535a9fbe8e55e72d021b39c
evgen <evgensudanov@mail.ru>
2024-12-09 23:18:40 +0300

    fish version_1.1

--- fish-and-beer/src/main/java/ru/sudanov/fish/and/beer/model/Event.java
+++ fish-and-beer/src/main/java/ru/sudanov/fish/and/beer/model/Event.java
@@ -16,7 +16,7 @@ import java.util.List;
 @EqualsAndHashCode
 public class Event {
     @Id
-    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column
     private String name;
